4e6ef2062d1c2bbf4ecc65735445b38e0a71b3a4
Alexandr <artamoshkinas@gmail.com>
2022-12-03 17:51:15 +0700

    Задание 3 дополнено

--- Home_Work_3.py
+++ Home_Work_3.py
@@ -1,1 +1,13 @@
+# Задайте последовательность чисел.
+# Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
 
+def sequence_of_numbers(list_a: list[int]) -> list[int]:
+    # Созжаем пустой список для уникальных чисел
+    list_elem = []
+    for i in list_a:
+        if list_a.count(i) == 1:
+            list_elem.append(i)
+    return list_elem
+
+
+print(sequence_of_numbers([1, 1, 2, 3, 4, 4, 4]))
