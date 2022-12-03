5bd65d7f5f4a065bc953afc40af817b6f07d01f9
Alexandr <artamoshkinas@gmail.com>
2022-12-03 14:35:26 +0700

    Задание 1 выполнено

--- Home_Work_1.py
+++ Home_Work_1.py
@@ -1,1 +1,7 @@
+import math
+#Вычислить число c заданной точностью d
+def PI_number_accuracy():
+    n = int(input("Введите количество знаков после запятой: "))
+    return f'- при d = {n}, {chr(960)} = {str(math.pi)[:n+2]}'
 
+print(PI_number_accuracy())
