bf7301db748642fc9d7e7a3598203c10392d34fe
Alexandr <artamoshkinas@gmail.com>
2022-12-03 14:38:50 +0700

    Задание 1 дополнено

--- Home_Work_1.py
+++ Home_Work_1.py
@@ -1,7 +1,10 @@
 import math
-#Вычислить число Пи c заданной точностью d
+
+
+# Вычислить число Пи c заданной точностью d
 def PI_number_accuracy():
     n = int(input("Введите количество знаков после запятой: "))
-    return f'- при d = {n}, {chr(960)} = {str(math.pi)[:n+2]}'
+    return f'- при d = {1 / (10 ** n)}, {chr(960)} = {str(math.pi)[:n + 2]}'
+
 
 print(PI_number_accuracy())
